b6bc2b6ba3f35a1758f09da9f91491d6280d60a6
s4nthiti <s4nthiti@gmail.com>
2026-05-16 12:26:40 +0700

    feat(integrate): update api url to azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://YOUR-DOTNET-API.azurewebsites.net'
+  apiBaseUrl: 'https://dotnetinterview-bnbvcyh2dke0fvff.southeastasia-01.azurewebsites.net/'
 };
